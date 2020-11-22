1cce2988e42fe244ebb1ee210ed63299f21c9e5f
silverysky <qazxdrtgbnj@gmail.com>
2020-11-22 17:05:33 +0900

    Add written/urls.py

--- written/written/urls.py
+++ written/written/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('user.urls')),
+    path('admin/', include('posting.urls')),
 ]
