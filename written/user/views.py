271cea4bba7bce16ccf32b6f62b487e95520f38f
shchoi94 <hyuklassic@gmail.com>
2020-12-24 12:31:53 +0900

    구독,구독취소[API Subscription] & error_code .py

--- written/user/views.py
+++ written/user/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from subscription.models import Subscription
+from written.error_codes import *
 
 
 # API User==================================================================
@@ -17,6 +20,23 @@ from rest_framework.decorators import action
 # POST /users/{user_id}/unsubscribe/
 # GET /users/subscribed/
 # GET /users/subscriber/
+
+def get_writer(pk):
+    try:
+        return User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return None
+
+
+def get_subscription(subscriber_id, writer_id, sub_or_unsub):
+    try:
+        return Subscription.objects.get(subscriber_id=1, writer_id=writer_id)
+    except Subscription.DoesNotExist:
+        if sub_or_unsub == "sub":
+            return Subscription.objects.create(subscriber_id=1, writer_id=writer_id, is_active=False)
+        else:
+            return None
+
 
 class UserViewSet(viewsets.GenericViewSet):
     # API User===============================================================
@@ -55,11 +75,29 @@ class UserViewSet(viewsets.GenericViewSet):
     # POST /users/{user_id}/subscribe/
     @action(detail=True, methods=['POST'], url_path='subscribe')
     def subscribe(self, request, pk):
+        subscriber_id = request.user.id
+        writer = get_writer(pk)
+        if writer is None:
+            return Response(error_10003, status=status.HTTP_400_BAD_REQUEST)
+        sb = get_subscription(subscriber_id, pk, "sub")
+        if sb.is_active:  # if True -> error
+            return Response(error_30001, status=status.HTTP_400_BAD_REQUEST)
+        sb.is_active = True
+        sb.save()
         return Response(status=status.HTTP_200_OK)
 
     # POST /users/{user_id}/unsubscribe/
     @action(detail=True, methods=['POST'], url_path='unsubscribe')
     def unsubscribe(self, request, pk):
+        subscriber_id = request.user.id
+        writer = get_writer(pk)
+        if writer is None:
+            return Response(error_10003, status=status.HTTP_400_BAD_REQUEST)
+        sb = get_subscription(subscriber_id, pk, "unsub")
+        if sb.is_active is False or sb is None:  # if False or None -> error
+            return Response(error_30002, status=status.HTTP_400_BAD_REQUEST)
+        sb.is_active = False
+        sb.save()
         return Response(status=status.HTTP_200_OK)
 
     # GET /users/subscribed/
---
+++ written/written/error_codes.py
@@ -0,0 +1,57 @@
+# 10000 : Users
+error_10001 = {
+    "errorcode": "10001",
+    "message": "Invalid facebook token"
+}
+error_10002 = {
+    "errorcode": "10002",
+    "message": "Nickname duplicate"
+}
+error_10003 = {
+    "errorcode": "10003",
+    "message": "User does not exist"
+}
+error_10004 = {
+    "errorcode": "10004",
+    "message": "User is not authorized"
+}
+
+# 20000 Postings
+error_20001 = {
+    "errorcode": "20001",
+    "message": "-"
+}
+error_20002 = {
+    "errorcode": "20002",
+    "message": "-"
+}
+error_20003 = {
+    "errorcode": "20003",
+    "message": "-"
+}
+error_20004 = {
+    "errorcode": "20004",
+    "message": "-"
+}
+
+# 30000 Subscriptions
+error_30001 = {
+    "errorcode": "30001",
+    "message": "User is already subscribed",
+}
+
+error_30002 = {
+    "errorcode": "30002",
+    "message": "User is not subscribed",
+}
+
+# 40000 Scraps
+error_40001 = {
+    "errorcode": "40001",
+    "message": "Posting is already scrapped",
+}
+
+error_40002 = {
+    "errorcode": "40002",
+    "message": "Posting is not scrapped",
+}
