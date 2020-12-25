18b4895b1c202dbe11842287ffc97c875102d7d7
silverysky <qazxdrtgbnj@gmail.com>
2020-12-25 20:25:07 +0900

    Minor change in posting/models

--- written/posting/models.py
+++ written/posting/models.py
@@ -4,15 +4,15 @@ from title import Title
 class Posting(models.Model):
     LEFT = 'LEFT'
     CENTER = 'CENTER'
-    ALIGNMENT = [
+    ALIGNMENT_CHOICES = [
         (LEFT, LEFT),
         (CENTER, CENTER),
     ]
     ALIGNMENTS = (LEFT, CENTER)
 
-    title = models.ForeignKey(Title, related_name="posting_title", on_delete=models.CASCADE)
-    writer = models.ForeignKey(User, related_name="posting_writer", on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, related_name="posting_title")
+    writer = models.ForeignKey(User, related_name="posting_writer")
     content = models.TextField()
-    alignment = models.CharField(choices=ALIGNMENT, default=LEFT)
+    alignment = models.CharField(choices=ALIGNMENT_CHOICES, default=LEFT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
