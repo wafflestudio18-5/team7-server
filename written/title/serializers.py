1d72fd1c78de0612c76a19e9d7a508a67f074462
silverysky <qazxdrtgbnj@gmail.com>
2020-12-30 11:33:49 +0900

    Add TitleSerializer validate()

--- written/title/serializers.py
+++ written/title/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from title.models import Title
 from posting.models import Posting
+from written.error_codes import *
 
 class TitleSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
@@ -17,7 +18,16 @@ class TitleSerializer(serializers.ModelSerializer):
             'name',
             'postings',
         )
-    
+    def validate(self, data):
+        name = data.get('name')
+        if name is None or name == '':
+            raise TitleNameIsEmptyException()
+        method = self.context['request'].method
+        if method == 'POST':
+            if Title.objects.filter(name=name):
+                raise TitleNameIsDuplicateException()
+        return data
+
     def get_postings(self, title):
         return title.postings.all()
 
