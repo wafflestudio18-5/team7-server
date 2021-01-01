b41dde66f745a0da7981804b3cf3f973b4610922
silverysky <qazxdrtgbnj@gmail.com>
2021-01-01 21:31:02 +0900

    Modify posting/models - add is_public

--- written/posting/models.py
+++ written/posting/models.py
@@ -11,8 +11,9 @@ class Posting(models.Model):
     ALIGNMENTS = (LEFT, CENTER)
 
     title = models.ForeignKey(Title, related_name="postings", on_delete=models.CASCADE)
-    writer = models.ForeignKey(User, related_name="postings", on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, related_name="postings", on_delete=models.CASCADE, null=True)
     content = models.TextField()
     alignment = models.CharField(choices=ALIGNMENT_CHOICES, default=LEFT, max_length=7)
+    is_public = models.BooleanField(default=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
