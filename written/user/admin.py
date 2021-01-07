9a482170ddef8fba7c128ef497150da64b3bdf84
shchoi94 <hyuklassic@gmail.com>
2021-01-07 14:42:16 +0900

    pagination modified

--- written/user/admin.py
+++ written/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nickname', 'description']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
